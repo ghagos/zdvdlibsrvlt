b44b95d3493cbe9879a20b645e7c5fbfcf182475
ghagos <ghagost@yahoo.com>
2015-08-30 07:50:46 -0700

    Implement Module 5 - Sharing Application Resources Using the Servlet Contexts

---
+++ src/java/com/zdvd/view/AddDVDFormServlet.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zdvd.view;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author GHagos
+ */
+public class AddDVDFormServlet extends HttpServlet {
+
+    private String [] genres;
+
+    @Override
+    public void init() throws ServletException {
+        String temp = getInitParameter("genre-list");
+        if (temp != null && temp.length() != 0) {
+            genres = temp.split(",");
+        }
+        super.init();
+    }
+    
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            
+            ArrayList<String> errors = (ArrayList) request.getAttribute("errors");
+            
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet AddDVDFormServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h3>Servlet AddDVDFormServlet at " + request.getContextPath() + "</h3>");
+            
+            if (errors != null && !errors.isEmpty()) {
+                out.println("<font color='red'>Please correct the following error(s):");
+                for (String error : errors) {
+                    out.println("<li>" + error + "</li>");
+                }
+                out.println("</font>");
+            }
+            out.println("<br>");
+            String title = request.getParameter("title");
+            String year = request.getParameter("year");
+            String newgenre = request.getParameter("newgenre");
+            String genre = request.getParameter("genre");
+            
+            out.println("<form action=\"adddvdservlet.do\" method=\"POST\">");
+            out.println("Title: <input type=\"text\" name=\"title\" value=\""+ (title == null ? "" : title) + "\"><br>");
+            out.println("Year: <input type=\"text\" name=\"year\" value=\""+ (year  == null ? "" : year) + "\"><br>");
+            out.println("Genre: ");
+            
+            out.println("<select name=\"genre\">");
+            for (String g : genres) {
+                out.println("<option value=\"" + g.trim() + "\""+ (g.trim().equals(genre) ? " selected" : "") + ">" + g.trim() + "</option>");
+            }
+            out.println("</select> ");
+            
+            out.println("or New Genre: <input type=\"text\" name=\"newgenre\" value=\""+ (newgenre  == null ? "" : newgenre) + "\"><br><br>");
+            out.println("<input type=\"submit\" value=\"Add DVD\">");
+            out.println("</form>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/com/zdvd/web/InitializeLibrary.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zdvd.web;
+
+import com.zdvd.model.DvdItem;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ *
+ * @author getachew
+ */
+public class InitializeLibrary implements ServletContextListener{
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        InputStream is = null;
+        BufferedReader br = null;
+        InputStreamReader isr = null;
+        
+        try {
+            List<DvdItem> dvdList = new ArrayList<>();
+
+            ServletContext context = sce.getServletContext();
+            String dataFile = context.getInitParameter("library-file");
+            is = sce.getServletContext().getResourceAsStream(dataFile);
+            
+            isr = new InputStreamReader(is);
+            br = new BufferedReader(isr);
+            
+            String line;        
+            while ((line = br.readLine()) != null) {
+                
+                String[] elements = line.split("\\|");
+                String title = elements[0];
+                String year = elements[1];
+                String genre = elements[2];
+                dvdList.add(new DvdItem(title, year, genre));
+            }
+            sce.getServletContext().setAttribute("dvdList", dvdList);
+            
+            // throw new UnsupportedOperationException("Not supported yet.");
+        } catch (IOException ex) {
+            Logger.getLogger(InitializeLibrary.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(InitializeLibrary.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (isr != null) {
+                try {
+                    isr.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(InitializeLibrary.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(InitializeLibrary.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
